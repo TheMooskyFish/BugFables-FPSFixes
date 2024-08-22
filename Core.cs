e4ff527634f319adc2d4022a5d10e68d00aa6754
TheMooskyFish <TheMooskyFish@gmail.com>
2024-08-22 03:49:07 +0300

    fix for mite knight's face camera

--- Core.cs
+++ Core.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace FPSFixes
 {
-    [BepInPlugin("dev.mooskyfish.FPSFixes", "FPS Fixes", "0.3.3.1")]
+    [BepInPlugin("dev.mooskyfish.FPSFixes", "FPS Fixes", "0.3.4")]
     [BepInProcess("Bug Fables.exe")]
     public class CorePlugin : BaseUnityPlugin
     {
---
+++ src/Patches/FaceCamera_P.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace FPSFixes.Patches
+{
+    [HarmonyPatch(typeof(FaceCamera))]
+    class FaceCamera_P
+    {
+        [HarmonyPatch("Start"), HarmonyPostfix]
+        static void Start(FaceCamera __instance)
+        {
+            if (__instance.gameObject.name == "player") // only for mite knight
+            {
+                __instance.gameObject.AddComponent<FaceCameraUpdate>().FaceCamera = __instance;
+                __instance.enabled = false;
+            }
+        }
+        class FaceCameraUpdate : MonoBehaviour
+        {
+            internal FaceCamera FaceCamera;
+            public void Update()
+            {
+                FaceCamera.FixedUpdate();
+            }
+        }
+    }
+}
