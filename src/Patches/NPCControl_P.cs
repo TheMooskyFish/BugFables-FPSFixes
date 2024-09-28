c6b36df89b2723118b835e3492a136352fdf6115
TheMooskyFish <TheMooskyFish@gmail.com>
2024-09-28 03:32:29 +0300

    Applied workaround for pushable ice cubes

--- src/Patches/NPCControl_P.cs
+++ src/Patches/NPCControl_P.cs
@@ -39,5 +39,39 @@ namespace FPSFixes.Patches
             .AddCustomDeltaTime(60f, 1, false, false)
             .InstructionEnumeration();
         }
+        [HarmonyPatch(typeof(NPCControl))]
+        static class PushableIceCube
+        {
+            [HarmonyPatch("FixedUpdate"), HarmonyPrefix]
+            static void FixedUpdatePos(NPCControl __instance)
+            {
+                if (MainManager.instance.pause || MainManager.instance.minipause) return;
+                if (__instance.objecttype == NPCControl.ObjectTypes.PushRock && __instance.data.Length == 3 && __instance.hit)
+                {
+                    __instance.internalvector[1] = __instance.entity.rigid.position;
+                    __instance.entity.rigid.position += __instance.internalvector[0] * (__instance.vectordata[0].z * 1.25f);
+                }
+            }
+            [HarmonyPatch("Update"), HarmonyTranspiler]
+            static IEnumerable<CodeInstruction> RemoveUpdatePos(IEnumerable<CodeInstruction> instructions)
+            {
+                var codematcher = new CodeMatcher(instructions);
+                codematcher.MatchForward(false,
+                    new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(NPCControl), nameof(NPCControl.ColliderNotThis)))
+                ).MatchForward(false,
+                    new CodeMatch(OpCodes.Ldarg_0),
+                    new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(NPCControl), nameof(NPCControl.internalvector))),
+                    new CodeMatch(OpCodes.Ldc_I4_1),
+                    new CodeMatch(OpCodes.Ldarg_0),
+                    new CodeMatch(OpCodes.Call)
+                ).Nopify(6)
+                .MatchForward(false,
+                    new CodeMatch(OpCodes.Ldarg_0),
+                    new CodeMatch(OpCodes.Call),
+                    new CodeMatch(OpCodes.Dup)
+                ).Nopify(17);
+                return codematcher.InstructionEnumeration();
+            }
+        }
     }
 }
