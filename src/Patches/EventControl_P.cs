1fadd7ffdc5793c0d4260ff0a9ab9c65191a1fd8
TheMooskyFish <TheMooskyFish@gmail.com>
2026-02-28 04:48:28 +0300

    remove commented code and add few chapter 1 events to safe events list

--- src/Patches/EventControl_P.cs
+++ src/Patches/EventControl_P.cs
@@ -6,32 +6,11 @@ namespace FPSFixes.Patches
     [HarmonyPatch(typeof(EventControl))]
     internal class EventControl_P
     {
-        //[HarmonyPatch(nameof(EventControl.Event68)), HarmonyPrefix]
-        //static void Event68Patch()
-        //{
-        //    Utils.ChangeInterpolation(MainManager.map.chompy, false);
-        //    foreach (var plr in MainManager.instance.playerdata)
-        //        Utils.ChangeInterpolation(plr.entity, false);
-        //}
-        //[HarmonyPatch(nameof(EventControl.EndEvent), [typeof(bool)]), HarmonyPrefix]
-        //static void EndEventPatch()
-        //{
-        //    switch (MainManager.lastevent)
-        //    {
-        //        case 68:
-        //            Utils.ChangeInterpolation(MainManager.map.chompy, true);
-        //            foreach (var plr in MainManager.instance.playerdata)
-        //                Utils.ChangeInterpolation(plr.entity, true);
-        //            return;
-        //        default:
-        //            return;
-        //    }
-        //}
-        private static readonly int[] s_safeEvents = [85, 200]; //TODO: check more events
+        private static readonly int[] SafeEvents = [2, 11, 14, 85, 200]; //TODO: check more events
         [HarmonyPatch(nameof(EventControl.StartEvent)), HarmonyPostfix]
         private static void StartEventPatch()
         {
-            var safeEventCheck = s_safeEvents.Contains(MainManager.lastevent);
+            var safeEventCheck = SafeEvents.Contains(MainManager.lastevent);
             CorePlugin.Logger.LogInfo($"Start Event: {MainManager.lastevent} - Safe Event Check: {safeEventCheck}");
             if (safeEventCheck) return;
             if (MainManager.map?.chompy)
@@ -42,7 +21,7 @@ namespace FPSFixes.Patches
         [HarmonyPatch(nameof(EventControl.EndEvent), typeof(bool)), HarmonyPostfix]
         private static void EndEventPatch()
         {
-            var safeEventCheck = s_safeEvents.Contains(MainManager.lastevent);
+            var safeEventCheck = SafeEvents.Contains(MainManager.lastevent);
             CorePlugin.Logger.LogInfo($"End Event: {MainManager.lastevent} - Safe Event Check: {safeEventCheck}");
             if (MainManager.map?.chompy)
                 Utils.ChangeInterpolation(MainManager.map?.chompy, true);
